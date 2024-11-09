089e1828c6af4ac2bbba1f9a18dddcc01fc28b2e
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2024-11-09 19:37:54 +0530

    feat: enhance RPC router with procedure handling and message processing

--- lib/bg-caller/types.ts
+++ lib/bg-caller/types.ts
@@ -1,15 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ControllerRecord = Record<string, (...args: any[]) => any>
-export type Arguments<J extends ControllerRecord, T> = T extends keyof J
-  ? Parameters<J[T]>
-  : never
+import { RPCRouter } from '.'
 
-export type Message<J extends ControllerRecord, T extends keyof J> = {
-  action: T
-  value: Arguments<J, T>
+export type Procedure<Args extends any[] = any[], Return = any> = (
+  ...args: Args
+) => Promise<Return> | Return
+
+export type ControllerRecord = {
+  [key: string]: Procedure | RPCRouter<any>
+}
+export interface Message<Action extends string, Args extends any[]> {
+  action: Action
+  value: Args
 }
 
-export type Caller<J extends ControllerRecord> = <T extends keyof J>(
-  action: T,
-  ...args: Arguments<J, T>
-) => Promise<NonNullable<Awaited<ReturnType<J[T]>>>>
+export type RPCActions<T extends RPCRouter<any>> = {
+  [K in keyof T['procedures']]: T['procedures'][K] extends Procedure<
+    infer A,
+    infer R
+  >
+    ? { action: K; args: A; return: R }
+    : T['procedures'][K] extends RPCRouter<infer U>
+      ? RPCActions<RPCRouter<U>> extends infer P
+        ? P extends { action: infer A; args: infer B; return: infer C }
+          ? { action: `${K & string}.${A & string}`; args: B; return: C }
+          : never
+        : never
+      : never
+}[keyof T['procedures']]
